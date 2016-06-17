f45a6ce6cb41e40f051fcc95683e1bf797a6f30e
juqi <qi.ju@qianhai.cn>
2016-06-17 14:06:25 +0800

    测试

--- survey/survey-web/src/main/java/com/jq/survey/web/controller/login/LoginController.java
+++ survey/survey-web/src/main/java/com/jq/survey/web/controller/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.jq.survey.web.controller.login;
 
+import org.springframework.stereotype.Controller;
+
 import com.jq.survey.web.controller.common.BaseController;
 
 /**
@@ -8,6 +10,7 @@ import com.jq.survey.web.controller.common.BaseController;
  * @author juqi
  * @version $Id: LoginController.java, v 0.1 2016年6月16日 下午3:28:38 juqi Exp $
  */
+@Controller
 public class LoginController extends BaseController {
 
 	/**  */
