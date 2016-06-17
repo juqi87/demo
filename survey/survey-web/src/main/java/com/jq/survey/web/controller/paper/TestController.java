dcb8992eb39582b396d42d0321b7b6a8aee1048b
juqi <qi.ju@qianhai.cn>
2016-06-17 14:06:25 +0800

    框架

--- survey/survey-web/src/main/java/com/jq/survey/web/controller/paper/TestController.java
+++ survey/survey-web/src/main/java/com/jq/survey/web/controller/paper/TestController.java
@@ -2,16 +2,18 @@ package com.jq.survey.web.controller.paper;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class TestController {
 
 	
 	@RequestMapping(value="test.htm", method = RequestMethod.GET)
 	public String dataQuestion(String path, Model model, HttpServletRequest request){
-		
+		System.out.println(11111111);
 		return "paper/dataQuestion";
 	}
 }
