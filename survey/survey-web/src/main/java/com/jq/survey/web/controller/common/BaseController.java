f5324af1743b3817bfe8c2cae9bcecfeeb37c9e1
juqi <qi.ju@qianhai.cn>
2016-06-17 12:35:04 +0800

    搭建框架

---
+++ survey/survey-web/src/main/java/com/jq/survey/web/controller/common/BaseController.java
@@ -0,0 +1,16 @@
+package com.jq.survey.web.controller.common;
+
+import java.io.Serializable;
+
+/**
+ * 基控制类
+ * 提供一些公共的方法
+ * @author juqi
+ * @version $Id: BaseController.java, v 0.1 2016年6月16日 下午3:27:41 juqi Exp $
+ */
+public class BaseController implements Serializable {
+
+	/**  */
+	private static final long serialVersionUID = -2605726791067096142L;
+
+}
