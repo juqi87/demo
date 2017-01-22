1edaa5f1afd4f6412577e02763a804ed60faf8b8
juqi <qi.ju@qianhai.cn>
2017-01-22 15:30:32 +0800

    as Change-Id: I6d35699a23db4e20ea9d164a854787acbc70c706

---
+++ wechat/src/org/jq/wechat/template/TemplateManager.java
@@ -0,0 +1,15 @@
+package org.jq.wechat.template;
+
+/**
+ * 发送模板消息
+ * 
+ * @author juqi
+ * @version $Id: TemplateManager.java, v 0.1 2017年1月13日 下午3:50:50 juqi Exp $
+ */
+public class TemplateManager {
+
+	public final static String model_template_send_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN";
+	
+	
+	
+}
