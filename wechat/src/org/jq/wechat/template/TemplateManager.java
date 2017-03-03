f024d6b1f8c62b25500270533b0669f4e0ac18d9
juqi <qi.ju@qianhai.cn>
2017-03-03 16:34:43 +0800

    Signed-off-by: juqi <qi.ju@qianhai.cn>

--- wechat/src/org/jq/wechat/template/TemplateManager.java
+++ wechat/src/org/jq/wechat/template/TemplateManager.java
@@ -1,5 +1,7 @@
 package org.jq.wechat.template;
 
+import org.apache.log4j.Logger;
+
 /**
  * 发送模板消息
  * 
@@ -7,6 +9,8 @@ package org.jq.wechat.template;
  * @version $Id: TemplateManager.java, v 0.1 2017年1月13日 下午3:50:50 juqi Exp $
  */
 public class TemplateManager {
+	
+	private static Logger logger = Logger.getLogger(TemplateManager.class);
 
 	public final static String model_template_send_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN";
 	
